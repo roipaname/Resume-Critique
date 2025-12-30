5e64180a905168d70310dcb1ebe30f5c7c81aeea
roipaname <roipaname@gmail.com>
2025-12-30 14:21:32 +0200

    set up

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import PyPDF2
+import io
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="Resume Critique",page_icon=":briefcase:",layout="centered")
